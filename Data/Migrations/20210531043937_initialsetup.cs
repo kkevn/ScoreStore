387012d8815cb13f5800069fbe1dde12f6e44440
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-05-30 23:55:16 -0500

    migrated and created databases for Player and Game models

---
+++ Data/Migrations/20210531043937_initialsetup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScoreStore.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    ImageURL = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayerModel",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    GameList = table.Column<string>(nullable: true),
+                    FriendList = table.Column<string>(nullable: true),
+                    StreakList = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+
+            migrationBuilder.DropTable(
+                name: "PlayerModel");
+        }
+    }
+}
