978faea3b5f1306c30b8ef9f84ab20db6804190d
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-06-26 16:41:17 -0500

    updated database with new user properties

---
+++ Data/Migrations/20210626213255_ExtendIdentityUser.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScoreStore.Data.Migrations
+{
+    public partial class ExtendIdentityUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FriendList",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GameList",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreakList",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FriendList",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GameList",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "StreakList",
+                table: "AspNetUsers");
+        }
+    }
+}
