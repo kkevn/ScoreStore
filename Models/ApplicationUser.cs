a909f340226603205ad4a3b3efd04f4eb69565c8
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-06-26 16:38:58 -0500

    added custom user model to extend default user properties

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoreStore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string GameList { get; set; }
+        public string FriendList { get; set; }
+        public string StreakList { get; set; }
+    }
+}
