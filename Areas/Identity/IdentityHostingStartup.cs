550298f21aaf6b29e8f9c41157f658dba05b4d4f
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-06-30 22:36:51 -0500

    added and adjusted default register page for custom username support

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ScoreStore.Data;
+using ScoreStore.Models;
+
+[assembly: HostingStartup(typeof(ScoreStore.Areas.Identity.IdentityHostingStartup))]
+namespace ScoreStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
