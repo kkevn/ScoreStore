2127bb1e6dc8564d7d0797424f4fb2b4491ca058
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2022-01-14 00:14:55 -0600

    redesigned Scores model supporting One-to-Many

--- Models/Scores.cs
+++ Models/Scores.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,43 +9,21 @@ namespace ScoreStore.Models
 {
     public class Scores
     {
-        public int UserId { get; set; }
-        public int GameId_1 { get; set; }
-        public int GameId_2 { get; set; }
-        public int GameId_3 { get; set; }
-        public int GameId_4 { get; set; }
-        public int GameId_5 { get; set; }
-        public int GameId_6 { get; set; }
-        public int GameId_7 { get; set; }
-        public int GameId_8 { get; set; }
-        public int GameId_9 { get; set; }
-        public int GameId_10 { get; set; }
-        public int GameId_11 { get; set; }
-        public int GameId_12 { get; set; }
-        public int GameId_13 { get; set; }
-        public int GameId_14 { get; set; }
-        public int GameId_15 { get; set; }
-        public int GameId_16 { get; set; }
-        public int GameId_17 { get; set; }
-        public int GameId_18 { get; set; }
-        public int GameId_19 { get; set; }
-        public int GameId_20 { get; set; }
-        public int GameId_21 { get; set; }
-        public int GameId_22 { get; set; }
-        public int GameId_23 { get; set; }
-        public int GameId_24 { get; set; }
-        public int GameId_25 { get; set; }
-        public int GameId_26 { get; set; }
-        public int GameId_27 { get; set; }
-        public int GameId_28 { get; set; }
-        public int GameId_29 { get; set; }
-        public int GameId_30 { get; set; }
-        public int GameId_31 { get; set; }
-        public int GameId_32 { get; set; }
+        public int Id { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+
+        public int GameId { get; set; }
+        [ForeignKey("GameId")]
+        public Game Game { get; set; }
 
         public Scores()
         {
-            
+            Wins = Losses = 0;
         }
     }
 }
