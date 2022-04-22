4f24e8bb9ba6c289c409b831a73ca1125f92c31a
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2022-04-21 21:20:14 -0500

    added ability for users to add to and view their own game list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ScoreStore.Data;
 using ScoreStore.Models;
 using System;
 using System.Collections.Generic;
@@ -14,11 +15,13 @@ namespace ScoreStore.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
             _logger = logger;
             _userManager = userManager;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -48,6 +51,8 @@ namespace ScoreStore.Controllers
                 return View(currentUser);
             }
         }
+
+        /**** FriendList Functions ****/
 
         public IActionResult SearchUsers()
         {
@@ -187,6 +192,105 @@ namespace ScoreStore.Controllers
 
                 // return FriendList view with model of subset of users with Id contained in friend list of current user
                 return View("FriendList", users.Where(u => currentUser.FriendList.Contains(u.Id)));
+            }
+        }
+
+        /**** GameList Functions ****/
+
+        public IActionResult GameList()
+        {
+            // obtain reference to currently logged in user by Id
+            var userId = _userManager.GetUserId(HttpContext.User);
+
+            // redirect user to log in if not already signed in
+            if (userId == null)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+            else
+            {
+                // obtain the user with Id
+                var currentUser = _userManager.FindByIdAsync(userId).Result;
+
+                // obtain list of all games in database
+                var games = _context.Game;
+
+                // return subset of games with Id contained in game list of current user
+                return View(games.Where(g => currentUser.GameList.Contains("{" + g.Id + "}")));
+            }
+        }
+
+        public async Task<IActionResult> AddGame()
+        {
+            // obtain the game Id to add from the route data
+            var gameId = Url.ActionContext.RouteData.Values["id"];
+
+            // obtain reference to currently logged in user by Id
+            var userId = _userManager.GetUserId(HttpContext.User);
+
+            // redirect user to log in if not already signed in
+            if (userId == null)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+            else
+            {
+                // obtain the user with Id
+                var currentUser = await _userManager.FindByIdAsync(userId);
+
+                // append selected game Id with curly braces to user's game list if it doesn't exist
+                string gameToAdd = "{" + gameId + "}";
+                if (currentUser.GameList != null) {
+                    if (!currentUser.GameList.Contains(gameToAdd))
+                        currentUser.GameList += gameToAdd;
+                }
+                else
+                    currentUser.GameList = gameToAdd;
+
+                // update user on database
+                await _userManager.UpdateAsync(currentUser);
+
+                // logging message for debugging purposes
+                System.Diagnostics.Debug.WriteLine("\t==> Current user {" + userId + "} added game {" + gameId + "} to user's game list");
+
+                // obtain list of all games in database
+                var games = _context.Game;
+
+                // return GameList view with model of subset of games with Id contained in game list of current user
+                return View("GameList", games.Where(g => currentUser.GameList.Contains("{" + g.Id + "}")));
+            }
+        }
+
+        public async Task<IActionResult> RemoveGame(String Id)
+        {
+            // obtain reference to currently logged in user by Id
+            var userId = _userManager.GetUserId(HttpContext.User);
+
+            // redirect user to log in if not already signed in
+            if (userId == null)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+            else
+            {
+                // obtain the user with Id
+                var currentUser = await _userManager.FindByIdAsync(userId);
+
+                // remove selected game Id with curly braces from user's game list
+                if (currentUser.GameList.Contains("{" + Id + "}"))
+                    currentUser.GameList = currentUser.GameList.Replace("{" + Id + "}", "");
+
+                // update user on database
+                await _userManager.UpdateAsync(currentUser);
+
+                // logging message for debugging purposes
+                System.Diagnostics.Debug.WriteLine("\t==> Current user {" + userId + "} removed game {" + Id + "} from user's game list");
+
+                // obtain list of all games in database
+                var games = _context.Game;
+
+                // return GameList view with model of subset of games with Id contained in game list of current user
+                return View("GameList", games.Where(g => currentUser.GameList.Contains("{" + g.Id + "}")));
             }
         }
 
