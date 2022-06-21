dca0abea7acd30338207c26b9b652b33d0f423e5
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2022-06-21 17:43:07 -0500

    added ability for emails to be sent via SendGrid

---
+++ Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoreStore.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string? ApiKey { get; set; }
+        public string? SenderEmail { get; set; }
+        public string? SenderName { get; set; }
+    }
+}
